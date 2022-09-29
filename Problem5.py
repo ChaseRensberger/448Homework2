c8aef6b2d026303c46687ff609fa8ade77a3b45a
ChaseRensberger <chasehrensberger@gmail.com>
2022-09-29 17:38:34 -0400

    a lot of progress

--- Problem5.py
+++ Problem5.py
@@ -3,9 +3,50 @@ import random
 import numpy as np
 
 
+def compGsubDelta(w, x, y, delta):
+    if y >= np.dot(np.transpose(w), x) + delta:
+        return (y - np.dot(np.transpose(w), x) - delta) ** 2
+    elif abs(y - np.dot(np.transpose(w), x)) < delta:
+        return 0
+    elif y <= np.dot(np.transpose(w), x) - delta:
+        (y - np.dot(np.transpose(w), x) + delta) ** 2
+    return -1
+
+def computeFunction(w, x, y, lam):
+    runningSum = 0
+    for x_i, y_i in zip(x, y):
+        runningSum += compGsubDelta(w, x_i, y_i, delta)
+    return (((runningSum)/len(x)) + (lam*(sum(w ** 2))))
+
+def computeFunctionGradient(w, x, y, lam, cond):
+    pass
+
+
+
 def bgd_l2(data, y, w, eta, delta, lam, num_iter):
-    return new_w, history_fw
+    history_fw = []
+    iteration = 0
+    # w = np.array([(0), (0)])
+    newW = w
+    while iteration < num_iter:
+        history_fw.append(computeFunction())
+        for x_i, y_i in zip(data, y):
+
+            newW = newW + eta * 
+        
+        iteration += 1
+
+   
+    
+    # return new_w, history_fw
 
 
 def sgd_l2(data, y, w, eta, delta, lam, num_iter, i=-1):
-    return new_w, history_fw
+    # return new_w, history_fw
+    pass
+
+bgd_l2(np.load("data.npy"), 0, 0, 0, 0, 0, 0)
+
+# A = np.array([(1), (4)])
+    # B = np.array([(1), (2)])
+    # print(np.dot(A,B))
--- Problem5Plot.py
+++ Problem5Plot.py
@@ -2,6 +2,27 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+# from Problem5 import bgd_l2
+
 if __name__ == '__main__':
-    # Put the code for the plots here, you can use different functions for each
-    # part
+    data = np.load("data.npy")
+    data = np.insert(data, 1, np.ones(len(data)), axis=1) # Add column of ones
+    # xValues = data[:, 0]
+    # yValues = data[:, 2]
+    # plt.scatter(xValues, yValues)
+    # plt.show()
+
+
+
+
+
+
+
+
+# year = [2014, 2015, 2016, 2017, 2018, 2019]  
+# tutorial_count = [39, 117, 111, 110, 67, 29]    
+
+# plt.plot(year, tutorial_count, color="#6c3376", linewidth=3)  
+# plt.xlabel('Year')  
+# plt.ylabel('Number of futurestud.io Tutorials') 
+# plt.savefig('line_plot.pdf') 
