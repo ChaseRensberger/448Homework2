0346f675b273315e01bbfc7947d4c1f42cb3ee80
ChaseRensberger <chasehrensberger@gmail.com>
2022-10-03 22:37:10 -0400

    almost done

--- Problem5.py
+++ Problem5.py
@@ -6,16 +6,18 @@ warnings.filterwarnings('ignore')
 
 
 def computeFunction(w, x, y, lam, delta):
+    # Used to compute the value of the objective function after each update to w
     n = len(x)
-    f = 0
+    objFunc = 0
     for x_i, y_i in zip(x, y):
         
         if y_i >= np.dot(np.transpose(w), x_i) + delta:
-            f += (((y_i-np.dot(np.transpose(w), x_i) - delta) ** 2)/n)
+            objFunc += (((y_i-np.dot(np.transpose(w), x_i) - delta) ** 2)/n)
+        # middle case just adds 0 so can be ignored
         elif y_i <= np.dot(np.transpose(w), x_i) - delta:
-            f += (((y_i-np.dot(np.transpose(w), x_i) + delta) ** 2)/n)
+            objFunc += (((y_i-np.dot(np.transpose(w), x_i) + delta) ** 2)/n)
     
-    return f + (lam * ((w[0] ** 2) + (w[1] ** 2)))
+    return objFunc + (lam * ((w[0] ** 2) + (w[1] ** 2)))
 
 
 def bgd_l2(data, y, w, eta, delta, lam, num_iter):
@@ -23,6 +25,7 @@ def bgd_l2(data, y, w, eta, delta, lam, num_iter):
     iteration = 1
     n = len(data)
     while iteration < num_iter + 1:
+        #pretty sure the best way to do this is with a variable number of w and you want to represent it as a vector but for the sake of getting this turned in i'm leaving it like this
         w0_grad = 0
         w1_grad = 0
 
@@ -32,7 +35,7 @@ def bgd_l2(data, y, w, eta, delta, lam, num_iter):
             if y_i >= np.dot(np.transpose(w), x_i) + delta:
                 w0_grad += -(2/n) * x_i[0] * (y_i - np.dot(np.transpose(w), x_i) - delta)
                 w1_grad += -(2/n) * (y_i - np.dot(np.transpose(w), x_i) - delta)
-
+            # middle case just adds 0 so can be ignored
             elif y_i <= np.dot(np.transpose(w), x_i) - delta:
                 w0_grad += -(2/n) * x_i[0] * (y_i - np.dot(np.transpose(w), x_i) - delta)
                 w1_grad += -(2/n) * (y_i - np.dot(np.transpose(w), x_i) - delta)
@@ -51,10 +54,38 @@ def bgd_l2(data, y, w, eta, delta, lam, num_iter):
 
 def sgd_l2(data, y, w, eta, delta, lam, num_iter, i=-1):
 
-        return new_w, history_fw
+    history_fw = []
+    iteration = 1
+    n = len(data)
+    while iteration < num_iter + 1:
+        w0_grad = 0
+        w1_grad = 0
 
+        history_fw.append(computeFunction(w, data, y, lam, delta))
+        if i == -1:
+            randPoint = random.randint(0, n)
+            x_i = data[randPoint]
+            y_i = y[randPoint]
+        else:
+            x_i = data[i]
+            y_i = y[i]
+            
+        if y_i >= np.dot(np.transpose(w), x_i) + delta:
+            w0_grad += -(2/n) * x_i[0] * (y_i - np.dot(np.transpose(w), x_i) - delta)
+            w1_grad += -(2/n) * (y_i - np.dot(np.transpose(w), x_i) - delta)
+        # middle case just adds 0 so can be ignored
+        elif y_i <= np.dot(np.transpose(w), x_i) - delta:
+            w0_grad += -(2/n) * x_i[0] * (y_i - np.dot(np.transpose(w), x_i) - delta)
+            w1_grad += -(2/n) * (y_i - np.dot(np.transpose(w), x_i) - delta)
+                
 
+        w0_grad += 2 * lam * w[0]
+        w1_grad += 2 * lam * w[1]
+        w[0] -= ((1/math.sqrt(iteration)) * w0_grad)
+        w[1] -= ((1/math.sqrt(iteration)) * w1_grad)
+        if i != -1:
+            break
+        iteration += 1
 
-# A = np.array([(1), (4)])
-    # B = np.array([(1), (2)])
-    # print(np.dot(A,B))
+    new_w = [w[0], w[1]]
+    return new_w, history_fw
--- Problem5Plot.py
+++ Problem5Plot.py
@@ -1,11 +1,18 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
-from Problem5 import bgd_l2
+from Problem5 import bgd_l2, sgd_l2
 
 def plotGD(data, y, w, eta, delta, lam, num_iter):
     new_w, history_fw = bgd_l2(data, y, w, eta, delta, lam, num_iter)
+    plt.plot(history_fw)
+    plt.xlabel('Iteration')
+    plt.ylabel('f(w)')
+    plt.title("BGD(η = 0.1,δ = 0.01,λ = 0.001,num iter = 50)")
+    plt.show()
+
+def plotSGD(data, y, w, eta, delta, lam, num_iter):
+    new_w, history_fw = sgd_l2(data, y, w, eta, delta, lam, num_iter)
     plt.plot(history_fw)
     plt.xlabel('Iteration')
     plt.ylabel('f(w)')
@@ -17,6 +24,15 @@ if __name__ == '__main__':
     xValues = data[:, :2]
     yValues = data[:, 2]
     plotGD(xValues, yValues, np.array([(0), (0)], dtype=float), 0.05, 0.1, 0.001, 50)
+    # plotGD(xValues, yValues, np.array([(0), (0)], dtype=float), 0.1, 0.01, 0.001, 50)
+    # plotGD(xValues, yValues, np.array([(0), (0)], dtype=float), 0.1, 0, 0.001, 100)
+    # plotGD(xValues, yValues, np.array([(0), (0)], dtype=float), 0.1, 0, 0, 100)
+
+    # plotSGD(xValues, yValues, np.array([(0), (0)], dtype=float), 1, 0.1, 0.5, 800)
+    # plotSGD(xValues, yValues, np.array([(0), (0)], dtype=float), 1, 0.01, 0.1, 800)
+    # plotSGD(xValues, yValues, np.array([(0), (0)], dtype=float), 1, 0, 0, 40)
+    # plotSGD(xValues, yValues, np.array([(0), (0)], dtype=float), 1, 0, 0, 800)
+
     
 
     
